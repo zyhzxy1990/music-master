4f34cf13403760ba944c7b72a0cb08a5af37e707
zyhzxy1990 <414833525@qq.com>
2020-08-14 15:26:33 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//add remark
+//add remark 11111
 const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 class App extends Component{
     state = {artistQuery : '', artist:null, tracks:[]};
