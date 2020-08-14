146d30b4a08567b9029ea908db8fa97d5af4161c
zyhzxy1990 <414833525@qq.com>
2020-08-14 15:24:05 +0800

    test

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -24058,7 +24058,6 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-//rrrr
 var API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
 var App = /*#__PURE__*/function (_Component) {
